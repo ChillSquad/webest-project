74deb7eec8b9f700fc30313112d5e69c6c42d031
Andrey <tyukachev2014@mail.ru>
2024-08-06 10:01:28 +0300

    add phone

---
+++ src/components/CompanyPhone.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  phone: {
+    type: [String, null],
+    default: null,
+  },
+});
+</script>
+
+<template>
+  <a v-if="phone" :href="`tel:${phone}`">{{ phone }}</a>
+</template>
